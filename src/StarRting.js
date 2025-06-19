40c6853090f5aa1eea0931c9d1bd76abda47281e
Iamkorir3 <korir5956@gmail.com>
2025-06-19 14:09:56 +0300

    new commit

--- src/StarRting.js
+++ src/StarRting.js
@@ -34,15 +34,14 @@ export default function StarRating({
     <div style={containerStyle}>
       <div style={starcontainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <Star
-            key={i}
+          <Star key={i}>
             onrate={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
             full={temprating ? temprating >= i + 1 : rating >= i + 1}
             color={color}
             size={size}
-          />
+          </Star>
         ))}
       </div>
       <p style={texStyle}>
@@ -54,28 +53,27 @@ export default function StarRating({
   );
 }
 
-function Star({ onrate, full, onHoverIn, onHoverOut, color, size }) {
+function Star({ children }) {
   const starStyle = {
-    width: `${size}`,
-    height: `${size}`,
+    width: `${children.size}`,
+    height: `${children.size}`,
     display: "block",
     cursor: "pointer",
-    color,
   };
 
   return (
     <span
       style={starStyle}
-      onClick={onrate}
-      onMouseEnter={onHoverIn}
-      onMouseLeave={onHoverOut}
+      onClick={children.onrate}
+      onMouseEnter={children.onHoverIn}
+      onMouseLeave={children.onHoverOut}
     >
-      {full ? (
+      {children.full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill={color}
-          stroke={color}
+          fill={children.color}
+          stroke={children.color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -84,7 +82,7 @@ function Star({ onrate, full, onHoverIn, onHoverOut, color, size }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke={color}
+          stroke={children.color}
         >
           <path
             strokeLinecap="round"
